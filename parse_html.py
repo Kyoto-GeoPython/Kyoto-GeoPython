12e71ee96f2ec831dd273d0621e0ac65341ba524
Yuki Miyake <zawawabis@gmail.com>
2017-03-16 14:13:26 +0900

    インデックスページをテンプレートで生成するコードを書いた。
    Signed-off-by: Yuki Miyake <zawawabis@gmail.com>

---
+++ parse_html.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+index.htmlを生成するスクリプト。
+
+"""
+
+import os
+from os.path import join
+from mistune import markdown
+
+dir_htmls = join("docs", "html")
+path_index = join("docs", "index.html")
+path_template = join("docs", "template.html")
+
+res = markdown("# KyotoGeoPython ホームページへようこそ")
+
+for folder in os.listdir(dir_htmls):
+    if os.path.isdir(folder):
+        res += markdown("## {}".format(folder))
+        for file in os.listdir(join(dir_htmls, folder)):
+            if file[-5:] == ".html":
+                name = file[:-5]
+                res += markdown("[{}]({})".format(name, join("html", folder, file)))
+        
+        
+with open(path_template, "r", encoding="utf-8") as fp:
+    template = "".join(fp.readlines())
+with open(path_index, "w", encoding="utf-8") as fp:
+    fp.write(template.format(res))
+print(res)
