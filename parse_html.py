7ddce87be8f88ec7c00b06537326e65999082372
Yuki Miyake <zawawabis@gmail.com>
2017-03-18 03:48:17 +0900

    リンク集の機能追加。parse_html.pyで自動生成。README参照。

--- parse_html.py
+++ parse_html.py
@@ -13,16 +13,52 @@ path_index = join("docs", "index.html")
 path_template = join("docs", "template.html")
 
 #res = markdown("# KyotoGeoPython ホームページへようこそ")
+
 res = ""
 
 for folder in os.listdir(dir_htmls):
     if os.path.isdir(folder):
+        reses = dict(basic="", other="")
         res += markdown("## {}".format(folder))
+        
         for file in os.listdir(join(dir_htmls, folder)):
             if file[-5:] == ".html":
                 name = file[:-5]
-                res += markdown("[{}]({})".format(name, join("html", folder, file)))
+                if file.startswith("Pythonの基礎"):
+                    key = "basic"
+                else:
+                    key = "other"
+                reses[key] += markdown("[{}]({})".format(name, join("html", folder, file)))
         
+        res += reses["basic"] + reses["other"]
+        
+# リンク集を生成する
+s_links = ""
+for file in sorted(os.listdir("リンク集")):
+    with open(join("リンク集", file), "r", encoding="utf-8_sig") as fp:
+        for line in fp.readlines():
+            s_links += markdown(line)
+
+style_sheet = """hr {
+  -moz-border-bottom-colors: none;
+  -moz-border-image: none;
+  -moz-border-left-colors: none;
+  -moz-border-right-colors: none;
+  -moz-border-top-colors: none;
+  border-color: #EEEEEE -moz-use-text-color #FFFFFF;
+  border-style: solid none;
+  border-width: 1px 0;
+  margin: 18px 0;
+}
+"""
+with open(join("docs", "template_Links.html"), "r", encoding="utf-8") as fp:
+    html_links = "".join(fp.readlines()).format(style_sheet, s_links)
+with open(join("docs", "html", "Links.html"), "w", encoding="utf-8") as fp:
+    fp.write(html_links)
+    
+# リンク集へのリンクを追加
+res += markdown("## その他")
+res += markdown("[リンク集]({})".format(join("html", "Links.html")))
         
 with open(path_template, "r", encoding="utf-8") as fp:
     template = "".join(fp.readlines())
---
+++ リンク集/Links_miyake.md
@@ -0,0 +1,8 @@
+### [Dive Into Python 3 日本語版](http://diveintopython3-ja.rdy.jp/)
+数値計算というよりWebサービスのために書かれているサイトですが、Python3の構文について分かりやすく学べます。  
+Python2との比較についても詳細に書かれています。
+***
+### [line_profiler and kernprof](https://github.com/rkern/line_profiler)
+Pythonプログラムの各行にかかる時間を計算するライブラリ。
+計算を遅くする部分（ボトルネック）を発見するために効果的です。
+***
