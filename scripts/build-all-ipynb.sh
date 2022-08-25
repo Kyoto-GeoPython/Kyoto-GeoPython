9a390489b2f0034d89c7ce5457fb5e29ed963ace
zawakin <zawawahoge@gmail.com>
2022-08-26 02:51:32 +0900

    save notebooks.lock

--- scripts/build-all-ipynb.sh
+++ scripts/build-all-ipynb.sh
@@ -2,4 +2,9 @@
 
 cd "$(dirname "$0")/.."
 
-find src/ipynb -name "*.ipynb" | grep -v "ipynb_checkpoints" | xargs -I {} -P 5 ./scripts/build-ipynb.sh {}
+source scripts/constants.sh
+
+notebooks="$(find "$src_base_dir" -type f -name "*.ipynb" | grep -v ".ipynb_checkpoints" | LC_ALL=C sort)"
+echo "$notebooks" > notebooks.lock
+
+echo "$notebooks" | xargs -I {} -P 5 ./scripts/build-ipynb.sh {}
